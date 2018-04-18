ebe64dcf347961157494574c9cc01803484d0a2a
Dovei <1679560428@qq.com>
2018-04-18 08:49:15 +0800

    Update 1.js

--- web/js/1.js
+++ web/js/1.js
@@ -1,6 +1,6 @@
 function ShowPic(a){
   var newaddress=a.getAttribute("href");
   document.getElementById("pic").src=newaddress;
-  var text=whichpic.getAttribute("title");
+  var text=a.getAttribute("title");
   document.getElementById("text").innerHTML=text;
 }
