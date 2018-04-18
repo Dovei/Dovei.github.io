b5cb86845b4f99c56b1f913abda0367d349fe5da
Dovei <1679560428@qq.com>
2018-04-18 08:46:13 +0800

    Update 1.js

--- web/js/1.js
+++ web/js/1.js
@@ -1,4 +1,6 @@
 function ShowPic(a){
   var newaddress=a.getAttribute("href");
   document.getElementById("pic").src=newaddress;
+  var text=whichpic.getAttribute("title");
+  document.getElementById("text").innerHTML=text;
 }
